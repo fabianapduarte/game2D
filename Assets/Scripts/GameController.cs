535bf6db4819fab24bab92dcfe3a7855d75de54f
Bianca Miranda <miranda.bianca41@gmail.com>
2023-05-09 01:55:13 -0300

    Pulo Duplo, Transição de animação (idle-running) e sistema de vidas

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     private int playerLifes = 5;
     private string sceneName;
     public static GameController instance = null;
+    public List<Image> hearts = new List<Image>();
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +21,10 @@ public class GameController : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    void DeadPlayer()
+    void HurtPlayer()
     {
         playerLifes--;
+        Destroy(hearts[hearts.Count-1]);
         Invoke("RestartLevel", 3f);
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float MaxSpeed;
-    public float JumpForce;
+    public float speed;
+    public float jumpForce;
+    public bool doubleJump;
 
     private Rigidbody2D rb;
     private SpriteRenderer rbSprite;
-    private bool PlayerInFloor = false;
+    private bool playerInFloor = false;
+    private float maxSpeed = 10f;
+    private Animator ani;
 
     public Transform detectFloor;
     public LayerMask isFloor;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        ani = GetComponent<Animator>();
         rbSprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
@@ -23,9 +29,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && PlayerInFloor == true)
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            if (playerInFloor)
+            {
+                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                playerInFloor = false;
+                doubleJump = true;
+            }else if (!playerInFloor && doubleJump == true){
+                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                playerInFloor = false;
+                doubleJump = false;
+            }
         }
 
     }
@@ -34,26 +49,41 @@ public class PlayerController : MonoBehaviour
     {
         // Movimentação
         float horizontal = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector3(MaxSpeed * horizontal, rb.velocity.y, 0f);
+        rb.velocity = new Vector3(speed * horizontal, rb.velocity.y, 0f);
         if(horizontal > 0)
         {
+            ani.SetBool("idle", true);
             rbSprite.flipX = false;
         }
-        if(horizontal < 0)
+        else if(horizontal < 0)
         {
+            ani.SetBool("idle", true);
             rbSprite.flipX = true;
         }
+        else
+        {
+            ani.SetBool("idle", false);
+        }
+        
 
         //Pulo Simples
-        PlayerInFloor = Physics2D.OverlapBox(detectFloor.position, new Vector2(0.16739f, 0.16739f), 0f, isFloor);
-        /*PlayerInFloor = Physics2D.OverlapCircle(detectFloor.position, 0.0887f, isFloor);*/
+        playerInFloor = Physics2D.OverlapBox(detectFloor.position, new Vector2(0.16739f, 0.16739f), 0f, isFloor);
+        /*playerInFloor = Physics2D.OverlapCircle(detectFloor.position, 0.0887f, isFloor);*/
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.layer.Equals("Floor"))
         {
-            PlayerInFloor = true;
+            playerInFloor = true;
+            doubleJump = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("collectibles")){
+            Destroy(collision.gameObject);
         }
     }
 }
