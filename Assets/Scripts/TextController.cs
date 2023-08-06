2528cdeb057f40636ce6dd989456242d0b31c7f9
Bianca Miranda <miranda.bianca41@gmail.com>
2023-08-06 17:23:42 -0300

    Cena de Créditos

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+    float speed = 0.25f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < 1000f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed, 0f);
+        }
+        else
+        {
+            Invoke("ResetCredits", 1f);
+        }
+    }
+ 
+    private void ResetCredits()
+    {
+        transform.position = new Vector3(transform.position.x, -900f, 0f);
+    }
+}
